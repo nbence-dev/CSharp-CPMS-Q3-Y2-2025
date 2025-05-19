79e492c479eb181a166fd8f128be4a913565629b
Nicholas <nicholasbence12@gmail.com>
2025-05-19 11:26:37 +0200

    Maybe refactor. Add a class? Add comments

--- Question_3/Program.cs
+++ Question_3/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, int> drones = new Dictionary<string, int>
+            {
+                { "1", 0 },
+                { "2", 0 },
+                { "3", 0 },
+                { "4", 0 },
+                { "5", 0 }
+            };
+
+            Random random = new Random();
+            List<string> keys = new List<string>(drones.Keys);
+            string winner = "";
+            bool foundWinner = false;
+            bool finishedRace = false;
+            while (!finishedRace)
+            {
+                string randomKey = keys[random.Next(keys.Count)];
+                if (drones[randomKey] < 25)
+                {
+                    drones[randomKey] += random.Next(1, 6);// Random speed between 1 and 3
+                    Console.WriteLine($"Drone {randomKey} covered: {drones[randomKey]} km");
+                    if (drones[randomKey] >= 25 && !foundWinner)
+                    {
+                        winner = randomKey;
+                        foundWinner = true;
+                    }
+                    finishedRace = true;
+                    foreach (var kvp in drones)
+                    {
+                        if (kvp.Value < 25)
+                        {
+                            finishedRace = false;
+                            break;
+                        }
+                    }
+                    Thread.Sleep(1000);
+                }
+                 
+                
+            }
+            Console.WriteLine($"Drone {winner} is the winner!");
         }
+
     }
 }
