bc65e69bdadfa58ec311c1b70b6b7a0c76915aeb
Nicholas <nicholasbence12@gmail.com>
2025-05-19 10:57:44 +0200

    Initial Version

---
+++ Question_3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
